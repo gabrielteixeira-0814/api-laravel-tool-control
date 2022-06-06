30abe1e50272e3f7bfbcffc010db55a5f106c711
gabrielteixeira-0814 <gabrielteixeira0814@gmail.com>
2022-06-06 20:33:06 -0300

    Criando repositorios e interface

---
+++ app/Repositories/TurnRepositoryInterface.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+interface TurnRepositoryInterface 
+{
+    public function __construct(Model $model);
+    public function store(array $data);
+    public function getList();
+    public function get($id);
+    public function update(array $data, $id);
+    public function destroy($id);
+}
+
+
+
+
+
+
+?>
---
+++ app/Services/TurnService.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace App\Services;
+use App\Repositories\TurnRepositoryInterface;
+
+
+class TurnService
+{
+    private $repo;
+
+    public function __construct(TurnRepositoryInterface $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    public function store(array $data)
+    {
+        return $this->repo->store($data);
+    }
+
+    public function getList()
+    {
+        return $this->repo->getList();
+    }
+
+    public function get($id)
+    {
+        return $this->repo->get($id);
+    }
+
+    public function update(array $data, $id)
+    {
+        return $this->repo->update($data, $id);
+    }
+
+    public function destroy($id)
+    {
+        return $this->repo->destroy($id);
+    }
+}
+
+
+
+
+?>
