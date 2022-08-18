f451122870cdb05cf244d34b0c37c8edd5843861
gabrielteixeira-0814 <gabrielteixeira0814@gmail.com>
2022-08-17 21:32:14 -0300

    Criando teste de autenticação de usuario

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -26,6 +26,101 @@ class AuthenticationTest extends TestCase
      * 
      */
     
+     /* Testar login e senha inserido de forma correta */
+    public function testMustEnterEmailAndPassword()
+    {
+        $this->json('post', 'api/login')
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "The email field is required. (and 1 more error)",
+                "errors" => [
+                    'email' => ["The email field is required."],
+                    'password' => ["The password field is required."],
+                ]
+            ]);
+    }
+
+    public function testSuccessfulLogin()
+    {
+
+        $turnId = Turn::create(
+            [
+                'turn' => 'primeiro',
+                'codeTurn' => '0010000',
+                'status' => 0
+            ]
+        );
+
+        $officeId = Office::create(
+            [
+                'office' => 'Analista',
+                'codeOffice' => '0010000',
+                'status' => 0
+            ]
+        );
+
+        $sectorId = Sector::create(
+            [
+                'sector' => 'Logistica',
+                'codeSector' => '0010000',
+                'status' => 0
+            ]
+        );
+
+        $userData = User::create(
+            [
+                "name" => "John Doe",
+                "email" => "doe@example.com",
+                "password" => bcrypt('demo12345'),
+                "password_confirmation" => bcrypt('demo12345'),
+                "cpf" => "00000000101",
+                "matricula" => "00000100",
+                "turn_id" => $turnId->id,
+                "office_id" => $officeId->id,
+                "sector_id" => $sectorId->id,
+            ]
+        );
+        
+        //dd($userData);
+
+        // $userData =
+        //     [
+        //         "name" => "John Doe",
+        //         "email" => "doe@example.com",
+        //         "password" => bcrypt('demo12345'),
+        //         "password_confirmation" => bcrypt('demo12345'),
+        //         "cpf" => "00000000101",
+        //         "matricula" => "00000100",
+        //         "turn_id" => $turnId->id,
+        //         "office_id" => $officeId->id,
+        //         "sector_id" => $sectorId->id,
+        //      ];
+            
+
+        $loginData = ['email' => 'doe@example.com', 'password' => 'demo12345'];
+
+        $this->json('post', 'api/login', $loginData)
+            ->assertStatus(201)
+            ->assertJsonStructure([
+               "user" => [
+                   'id',
+                   'name',
+                   'email',
+                   'cpf',
+                   'matricula',
+                   'turn_id',
+                   'office_id',
+                   'sector_id',
+                   'email_verified_at',
+                   'created_at',
+                   'updated_at',
+               ],
+               "tokenn"
+            ]);
+
+        $this->assertAuthenticated($userData);
+    }
+
 
      /* Teste que retornar as mensagem de erros quando o usuario nao passa os valores corretos para os campos */
     public function test_Required_Fields_For_Registration_users()
@@ -74,6 +169,11 @@ class AuthenticationTest extends TestCase
             ]);
     }
 
+
+
+
+
+
     /* Testa a criação de um registro de usuário */
     public function test_successful_registration()
     {
@@ -101,21 +201,7 @@ class AuthenticationTest extends TestCase
             ]
         );
 
-        // $user = User::create(
-        //    [
-        //         "name" => "John Doe",
-        //         "email" => "doe@example.com",
-        //         "password" => "demo12345",
-        //         "password_confirmation" => "demo12345",
-        //         "cpf" => "00000000101",
-        //         "matricula" => "00000100",
-        //         "turn_id" => $turnId->id,
-        //         "office_id" => $officeId->id,
-        //         "sector_id" => $sectorId->id,
-        //     ]
-        // );
-
-
+    
         $userData =
             [
                 "name" => "John Doe",
@@ -150,12 +236,6 @@ class AuthenticationTest extends TestCase
     }
 
 
-
-
-
-
-
-
      /***  Verificar se o status é 200 quando  traz uma lista de usuários.  ***/ 
      /***  Verificar se a estrutura da resposta em json é a correta. ***/ 
     public function test_returns_a_list_of_users() {
@@ -180,81 +260,5 @@ class AuthenticationTest extends TestCase
                  ]
              );
       }
-
-    
-      // Criar um usuário:
-      // Testa se o returno do status é (201)
-      // Testa se a estrutura do retorno do json é a correta
-      /**
-       * @
-       */
-    public function test_successful_create_user()
-    {
-
-        $user = [
-                "name" => "John Doeaaa", 
-                "email" => "doe@example.com",
-                "password" => "demo12345",
-                "password_confirmation" => "demo12345",
-                "cpf" => "12345657357",
-                "matricula" => "1234564387387",
-                "turn_id" => 1,
-                "office_id" => 1,
-                "sector_id" => 1,
-            ];
-            
-        $this->json('post', 'api/register', $user)
-            ->assertStatus(201)
-            ->assertJson(
-                [
-                    "user" => [
-                        "name" => "John Doeaaa", 
-                        "email" => "doe@example.com",
-                        "password" => "demo12345",
-                        "password_confirmation" => "demo12345",
-                        "cpf" => "12345657357",
-                        "matricula" => "1234564387387",
-                        "turn_id" => 1,
-                        "office_id" => 1,
-                        "sector_id" => 1,
-                    ],
-                    "token" => ""
-                ]);
-    }
-
-     // Retorna um status de criação (2021)
-     // Espera o retorno do json quando criar um usuário exatamente igual
-     public function test_user_create_api_request()
-     {
- 
-        $userData = [
-            "name" => "John Doeaaa", 
-            "email" => "doe@example.com",
-            "password" => "demo12345",
-            "password_confirmation" => "demo12345",
-            "cpf" => "12345657357",
-            "matricula" => "1234564387387",
-            "turn_id" => 1,
-            "office_id" => 1,
-            "sector_id" => 1,
-        ];
- 
-         $response = $this->postJson('/api/user', $userData);
-  
-         $response
-             ->assertStatus(201)
-             ->assertJson([
-                "name" => "John Doeaaa", 
-                "email" => "doe@example.com",
-                "password" => "demo12345",
-                "password_confirmation" => "demo12345",
-                "cpf" => "12345657357",
-                "matricula" => "1234564387387",
-                "turn_id" => 1,
-                "office_id" => 1,
-                "sector_id" => 1,
-             ]);
-     }
-
 
 }
